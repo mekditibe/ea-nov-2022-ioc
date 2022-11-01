f4702b8a5a9a02cfa056894d8b16672a9a1c528d
mekdes tebebu <mekditibe@gmail.com>
2022-10-31 19:37:20 -0500

    lab1

---
+++ Lab_1/src/main/java/com/example/lab_1/Lab1Application.java
@@ -0,0 +1,14 @@
+package com.example.lab_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab1Application.class, args);
+        System.out.println("Hello .............");
+    }
+
+}
